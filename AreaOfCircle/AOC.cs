08c42c9552ad0181780ec7984f559241c1015530
SahilGyanchandani <gyanchandanisahil8@gmail.com>
2023-05-09 14:06:55 +0530

    Second Commit

--- AreaOfCircle/AOC.cs
+++ AreaOfCircle/AOC.cs
@@ -3,28 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace AreaOfCircle
 {
     internal class AOC
     {
+        double Radius;
+        
         static void Main(string[] args)
         {
-            decimal radius = 0;
-            decimal area = 0;
-            double pi = 0;
+            double radius = 0;
+            Console.WriteLine("Enter the value of radius:");
+            radius = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter the radius of circle");
-            radius = Convert.ToDecimal(Console.ReadLine());
 
-            pi = 3.14;
+            AOC circle = new AOC(radius);
 
-            area = Convert.ToDecimal(pi) * radius * radius;
-            Console.WriteLine("Area of the circle with radius {0} is {1}", radius, area);
-            Console.WriteLine("Press any key to Exit");
-            Console.ReadKey();
+            Console.WriteLine("Area Of Circle: "+ circle.getarea());
+            Console.WriteLine("Aree of circumference: "+ circle.circumference());
 
         }
-        
+        public AOC(double radius)
+        {
+            this.Radius=radius;
+        }
+        public double getarea()
+        {
+            double pi = 0;
+            double area = 0;
+
+            pi = 3.14;
+            area = (pi) * this.Radius * this.Radius;
+            return area;
+        }
+
+        public double circumference()
+        {
+            double circumference = 0;
+            double pi = 0;
+
+            pi = 3.14;
+            circumference=2*pi*this.Radius;
+            return circumference;
+
+        }
     }
 }
